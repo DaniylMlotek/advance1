f97ee469ab069424f113d38c8eeb79dbf074514d
DaniylMlotek <91188195+DaniylMlotek@users.noreply.github.com>
2021-10-14 21:09:06 +0300

    Update github.py

--- github.py
+++ github.py
@@ -1,24 +1,14 @@
 while True:
-    alf_eng = 'absdefghijklmnopqrstuvwxyzabsdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWABCDEFGHIJKLMNOPQRSTUVW'
-    alf_ua = 'aбвгґдуєжзиіїйкльмншщпрстуфхцчшщьюяабвгґдуєжзиіїйкльмншщпрстуфхцчшщьюяАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ'
-    step = int(input('enter a key:'))
-    lang = input('enter a language eng/ua:')
+    alf_ua = 'aбвгґдуєжзиіїйкльмншщпрстуфхцчшщьюяабвгґдуєжзиіїйкльмншщпрстуфхцчшщьюяАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯabsdefghijklmnopqrstuvwxyzabsdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWABCDEFGHIJKLMNOPQRSTUVW1234567890'
+    step = 1
     message = input('message deshif:')
     result = ''
-    if lang == 'eng':
-        for i in message:
-            cell = alf_eng.find(i)
-            new_cell = cell + step
-            if i in alf_eng:
-                result += alf_eng[new_cell]
-            else:
-                result +=i
-    else:
-        for i in message:
-            cell = alf_ua.find(i)
-            new_cell = cell + step
-            if i in alf_ua:
-                result +=alf_ua[new_cell]
-            else:
-                result +=i
+    for i in message:
+        cell = alf_ua.find(i)
+        new_cell = cell + step
+        if i in alf_ua:
+            result += alf_ua[new_cell]
+        else:
+
+            result += i
     print(result)
